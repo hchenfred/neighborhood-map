4b67b73b727c8d43752dc9a9f21ef6ef34d152a7
Huan Chen <hchen.fred@gmail.com>
2016-10-14 10:51:35 -0700

    feat: add markers on Google Maps with Yelp search results

--- README.md
+++ README.md
@@ -4,7 +4,11 @@
 ## Development Steps
 
 * add a search box using Google Place API. [link](https://developers.google.com/maps/documentation/javascript/examples/places-searchbox)
-* add yelp search API. 
+* add Yelp search API. 
+* add an input element to allow users to enter a location used for Yelp search. location is a knockout observable.
+* enable autocomplete with Google Place Autocomplete API. [link](https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete)
+* add markers on Google Map using the results from Yelp search.
+
 
 
 
@@ -13,8 +17,11 @@
 
 # Google Places API
 
-* include "libraires=places"
+* To include "libraires=places"
 ```
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyChkGJbVaG7HZWDAiSsyXe_7mupndlvMyU&libraries=places&callback=initMap"
     async defer></script>
 ```
+* To use a knockout observable, it should be used like a function. For example, instead of this.location, this.location() should be used.
+
+* To assign observable a value, must use ko.observable(valueToBeAssigned)
--- js/app.js
+++ js/app.js
@@ -1,129 +1,141 @@
 //** Model **//
 var map;
+var place = {};
 
 //** ViewModel **//
 var initMap = function() {
-       // TODO: use a constructor to create a new map JS object. You can use the coordinates
-       // we used, 40.7413549, -73.99802439999996 or your own!
           map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 37.7779436, lng: -122.4152976},  
           zoom: 14
         });
 
-          // Create the search box and link it to the UI element.
-        var input = document.getElementById('places-search');
-        console.log(input);
-        var searchBox = new google.maps.places.SearchBox(input);
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
+
+        var input = /** @type {!HTMLInputElement} */(
+            document.getElementById('pac-input'));
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
         });
 
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
-          console.log(places);
-
-          if (places.length == 0) {
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          place = autocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
             return;
           }
 
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
-          markers = [];
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+            map.setZoom(15);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(15);  
+          }
+          marker.setIcon(/** @type {google.maps.Icon} */({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+          }));
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
 
-          // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            if (!place.geometry) {
-              console.log("Returned place contains no geometry");
-              return;
-            }
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
 
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-        });	        
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+          infowindow.open(map, marker);
+        });
 };
 
-var yelpRequest = function() {
-	/**
-	 * Generates a random number and returns it as a string for OAuthentication
-	 * @return {string}
-	 */
-
-	function nonce_generate() {
-	  return (Math.floor(Math.random() * 1e12).toString());
-	}
-
-	var yelp_url = 'https://api.yelp.com/v2/search?';
-
-	  var parameters = {
-	    term: 'food',
-	    location: 'san francisco',
-	    oauth_consumer_key: 'XPzZeqwKT1WcLNLZ-quVHA',
-	    oauth_token: '_HQnAhO1FXLMlv4c7Ivq231XQKyqais4',
-	    oauth_nonce: nonce_generate(),
-	    oauth_timestamp: Math.floor(Date.now()/1000),
-	    oauth_signature_method: 'HMAC-SHA1',
-	    callback: 'cb'             // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
-	  };
-	  
-	  var consumer_secret = 'oAAm0z1rqmaFFLQlXrZie7KqOl0',
-	      token_secret = 'ML0TTxj99Ixci3f1P46lsYlKKTc';
-	      
-	  var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, consumer_secret, token_secret);
-	  parameters.oauth_signature = encodedSignature;
-
-	  var settings = {
-	    url: yelp_url,
-	    data: parameters,
-	    cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
-	    dataType: 'jsonp',
-	    jsonpCallback: 'cb',
-	    success: function(results) {
-	      // Do stuff with results
-	      console.log("SUCCCESS! %o", results);
-	    },
-	    error: function(error) {
-	      // Do stuff on fail
-	      console.log(error);
-	    }
-	  };
-
-	// Send AJAX query via jQuery library.
-	$.ajax(settings);
-};
-
-yelpRequest();
 
 var ViewModel = function() {
     var self = this;
-   
+    self.location = ko.observable('');
+    self.yelpRequest = function() {
+      function nonce_generate() {
+        return (Math.floor(Math.random() * 1e12).toString());
+      }
+      
+      self.location = ko.observable(place.formatted_address);
+
+      var yelp_url = 'https://api.yelp.com/v2/search?';
+      var parameters = {
+        term: 'food',
+        location: self.location(),
+        oauth_consumer_key: 'XPzZeqwKT1WcLNLZ-quVHA',
+        oauth_token: '_HQnAhO1FXLMlv4c7Ivq231XQKyqais4',
+        oauth_nonce: nonce_generate(),
+        oauth_timestamp: Math.floor(Date.now()/1000),
+        oauth_signature_method: 'HMAC-SHA1',
+        callback: 'cb'             // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
+      };
+      
+      var consumer_secret = 'oAAm0z1rqmaFFLQlXrZie7KqOl0',
+          token_secret = 'ML0TTxj99Ixci3f1P46lsYlKKTc';
+          
+      var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters, consumer_secret, token_secret);
+      parameters.oauth_signature = encodedSignature;
+      var pinIcon = new google.maps.MarkerImage(
+          "img/food.png",
+          null, /* size is determined at runtime */
+          null, /* origin is 0,0 */
+          null, /* anchor is bottom center of the scaled image */
+          new google.maps.Size(40, 40)
+      );  
+
+      function processYelpResults(results) {
+        var businesses = results.businesses;
+          for (var i = 0; i < businesses.length; i++) {
+            var currBusiness = businesses[i];
+            var currLocation = currBusiness.location.coordinate;
+            var coordinates = {lat: currLocation.latitude, lng: currLocation.longitude};
+            var marker = new google.maps.Marker({
+                map: map,
+                icon: pinIcon,
+                draggable: true,
+                animation: google.maps.Animation.DROP,
+                position: coordinates
+            });
+          }
+      }
+
+      var settings = {
+        url: yelp_url,
+        data: parameters,
+        cache: true,                // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
+        dataType: 'jsonp',
+        jsonpCallback: 'cb',
+        success: function(results) {
+          console.log("SUCCCESS! %o", results);
+          processYelpResults(results);
+          
+        },
+        error: function(error) {
+          alert('Yelp API response error' + error);
+        }
+      };
+
+      // Send AJAX query via jQuery library.
+      $.ajax(settings);
+    };
+
+
 
 
 
